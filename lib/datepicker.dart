761154c4a36ff2d8bb2bbd44bda55059da684dab
karthi965 <bscit9706@gmail.com>
2024-04-24 03:41:45 -0700

    Initial commit

---
+++ lib/datepicker.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class DatePickerPage extends StatefulWidget {
+  @override
+  _DatePickerPageState createState() => _DatePickerPageState();
+}
+
+class _DatePickerPageState extends State<DatePickerPage> {
+  DateTime _selectedDate = DateTime.now();
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: _selectedDate,
+      firstDate: DateTime(1900),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != _selectedDate) {
+      setState(() {
+        _selectedDate = picked;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Date Picker Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Selected Date: ${_selectedDate.day}/${_selectedDate.month}/${_selectedDate.year}',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _selectDate(context),
+              child: Text('Select Date'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
